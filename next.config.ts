779531d09ccbea6037591aeb4b32247a176b8d67
Damian Frizzi <damianfrizzi@gmail.com>
2025-11-28 13:08:43 +0100

    Fix

--- next.config.ts
+++ next.config.ts
@@ -4,26 +4,6 @@ const nextConfig: NextConfig = {
     images: {
         loader: 'custom',
         loaderFile: './lib/imageLoader.ts',
-        imageSizes: [],
-        deviceSizes: [640, 820, 1200, 2432],
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'image.tmdb.org',
-            },
-            {
-                protocol: 'https',
-                hostname: 'www.theoneswelove.ch',
-            },
-            {
-                protocol: 'https',
-                hostname: 'sjnachksoziczhtscjte.supabase.co',
-            },
-            {
-                protocol: 'https',
-                hostname: 'xtoghadwmvopttvezlua.supabase.co',
-            },
-        ],
     },
 };
 
