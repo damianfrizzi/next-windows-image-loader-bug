f6bdf77d1fd7152c0cb0e20f4db289155b9bee62
Damian Frizzi <damianfrizzi@gmail.com>
2025-11-28 12:30:24 +0100

    Fix

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+    <div className="relative flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <Image
         fill
         src="https://static.theoneswelove.ch/assets/images/movies/The-Big-Lebowski-1998/still/The-Big-Lebowski-1998-still-1726071539543-0.jpg"
